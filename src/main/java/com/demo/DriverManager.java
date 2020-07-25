6535e70dd420987c35768dddeb76f9d75c326593
Subhradip Sinha <subhradip.hr.2coms@gmail.com>
2020-07-26 03:52:11 +0530

    third firefox driver

--- src/main/java/com/demo/DriverManager.java
+++ src/main/java/com/demo/DriverManager.java
@@ -3,6 +3,7 @@ package com.demo;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
@@ -21,15 +22,18 @@ public class DriverManager extends ConfigarationManager {
         ConfigarationManager.conficfile();
         String Url = ConfigarationManager.config.getProperty("URLTest");
         String browsername = ConfigarationManager.config.getProperty("browser");
-        if (browsername.equalsIgnoreCase("Chrome")) {
+        if (browsername.equalsIgnoreCase("Firefox")) {
 
-            System.setProperty("webdriver.chrome.driver", "E:\\sw\\Homework1\\amazone-master\\Driver2\\chromedriver.exe");
-            driver = new ChromeDriver();
+            System.setProperty("webdriver.gecko.driver", "E:\\sw\\Homework1\\amazone-master\\Driver2\\geckodriver.exe");
+            System.out.println("hit firefox");
+            driver = new FirefoxDriver();
+            Thread.sleep(5000);
             driver.manage().window().maximize();
 
         }
         driver.get(Url);
         System.out.println("URL HIT");
+        Thread.sleep(5000);
 
 //        WebDriverWait wait = new WebDriverWait(driver,120);
 //        wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//input[@placeholder='Enter User Name']")));

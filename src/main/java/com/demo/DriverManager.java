c1e165873cc87f19b949971a008221bed4fc5d2b
Subhradip Sinha <subhradip.hr.2coms@gmail.com>
2020-09-04 02:02:13 +0530

    Twenty six

--- src/main/java/com/demo/DriverManager.java
+++ src/main/java/com/demo/DriverManager.java
@@ -27,7 +27,7 @@ public class DriverManager extends ConfigarationManager {
         String browsername = ConfigarationManager.config.getProperty("browser");
         if (browsername.equalsIgnoreCase("Chrome")) {
 
-            System.setProperty("webdriver.chrome.driver", "E:\\sw\\Homework1\\amazone-master\\Driver3\\chromedriver.exe");
+            System.setProperty("webdriver.chrome.driver", "E:\\sw\\Homework1\\amazone-master\\Driver\\chromedriver.exe");
             System.out.println("hit Chrome Driver");
             driver = new ChromeDriver();
             Thread.sleep(5000);
@@ -38,10 +38,6 @@ public class DriverManager extends ConfigarationManager {
         driver.get(Url);
         System.out.println("URL HIT");
         Thread.sleep(5000);
-
-//        WebDriverWait wait = new WebDriverWait(driver,120);
-//        wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//input[@placeholder='Enter User Name']")));
-//        System.out.println("URL Open");
 
     }
 }

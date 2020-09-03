b79aa754b012da9bc1e6e72d8b6f74ed4ecdb389
Subhradip Sinha <subhradip.hr.2coms@gmail.com>
2020-09-04 00:56:27 +0530

    twenty two commit

--- src/main/java/com/demo/DriverManager.java
+++ src/main/java/com/demo/DriverManager.java
@@ -3,9 +3,12 @@ package com.demo;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.io.File;
 
 /**
  * @Author Subhradip sinha
